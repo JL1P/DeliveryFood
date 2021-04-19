eaa31346ffaad5fd572f859301fdab06837b7a22
JL1P <jluisar13@gmail.com>
2021-04-19 08:26:36 -0500

    init

---
+++ food/deliveryfood/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+
+router.register('pedido', views.PedidoViewSet, basename='pedidos')
+router.register('mensajero', views.MensajeroViewSet, basename='mensajeros')
+router.register('producto', views.ProductoViewSet, basename='productos')
+router.register('categoria', views.CategoriaViewSet, basename='categorias')
+router.register('administrador', views.AdministradorViewSet, basename='administradores')
+router.register('usuario', views.UsuarioViewSet, basename='usuarios')
+router.register('restaurante', views.RestauranteViewSet, basename='restaurantes')
+router.register('reporte_all', views.ReporteAllViewSet, basename='reporte_alls')
+router.register('reporte', views.ReporteViewSet, basename='reportes')
+router.register('valoracion', views.ValoracionViewSet, basename='valoraciones')
+
+urlpatterns = [
+    path('', include(router.urls))
+]
