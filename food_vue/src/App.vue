b0c415f45684754d215ba5609f4a8dedad713875
JL1P <jluisar13@gmail.com>
2021-05-18 20:48:33 -0400

    merge 2

--- food_vue/src/App.vue
+++ food_vue/src/App.vue
@@ -1,67 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <v-app>
-    <v-app-bar
-      absolute
-      elevate-on-scroll
-    >
-      <v-app-bar-nav-icon class="ml-2 mr-2 pb-1">
-        <img src="@/assets/lff.png" style="width:50px; height: 50px" alt="">
-      </v-app-bar-nav-icon>
-
-      <div id="menu">
-        <v-item>
-          <v-btn ml-2 elevation="0">
-            <strong class="navs-item" style="font-size:25px;">Restaurante</strong>
-          </v-btn>
-        </v-item>
-        <v-item>
-          <v-btn ml-2 elevation="0">
-            <strong class="navs-item" style="font-size:25px;">Repartidor</strong>
-          </v-btn>
-        </v-item>
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        color=#ffdf4d
-        elevation="0"
-        id="ordenar"
-      ><strong class="pedirOrden" style="color: white; font-style: italic">Realizar una orden</strong></v-btn>
-
-      <div class="burger-menu">
-        <v-menu rounded="b-xl" offset-y>
-          <template v-slot:activator="{ attrs, on }">
-              <v-btn 
-                class="white--text ma-5"
-                v-bind="attrs"
-                v-on="on"
-                  >
-                <v-app-bar-nav-icon color="#00D1B2"></v-app-bar-nav-icon>
-                <!-- Menu -->
-              </v-btn>
-          </template>
-          <v-list>
-          <v-list-item
-            v-for="item in items"
-            :key="item"
-            link
-              >
-              <v-list-item-title v-text="item"></v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-menu>
-      </div>
-
-
-    </v-app-bar>
-    
-    <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
-=======
   <div id="app">
 
     <b-navbar id="myNavbar" toggleable="lg" type="dark" class="py-0">
@@ -69,37 +6,37 @@
       <b-navbar-brand href="/">
         <img src="./assets/Home/logo.png" alt="logo" style="width: 40px">
       </b-navbar-brand>
-    
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        
+      </b-collapse>
+
     </b-navbar>
 
     <router-view/>
   </div>
->>>>>>> main
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  data() {
-    return {
-      btns: [
-        ['Removed', '0'],
-        ['Large', 'lg'],
-        ['Custom', 'b-xl'],
-      ],
-      colors: ['deep-purple accent-4', 'error', 'teal darken-1'],
-      items: [...Array(4)].map((_, i) => `Item ${i}`),
-    }
-  },
-};
+
+data() {
+  return {
+   mostrar: false, 
+  }
+},
+methods: {
+  showMenu() {
+    this.mostrar = !this.mostrar
+  }
+},
+}
 </script>
 
 <style lang="scss">
 
-<<<<<<< HEAD
-  .burger-menu {
-=======
 @import "./assets/scss/main";
 
 #myNavbar {
@@ -145,32 +82,17 @@ export default {
 
 @keyframes navAnim {
   0% {
->>>>>>> main
     display: none;
+    opacity: 0;
+    height: 0;
   }
-
-  .navs-item {
-    color: #00D1B2;
-    font-family: 'Times New Roman', Times, serif; 
-    font-style: italic
+  1% {
+    display: block;
+    opacity: 0;
   }
-<<<<<<< HEAD
-  
-  @media only screen and (max-width: 800px) {
-    #menu {
-      display: none
-    }
-    #ordenar {
-      display: none;
-    }
-    .burger-menu {
-      display: block;
-    }
-=======
   100% {
     opacity: 1;
     height: 116px;
->>>>>>> main
   }
-
+}
 </style>
